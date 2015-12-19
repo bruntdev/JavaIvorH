f85e80d388e382b61024573668752b62047a098a
Sky Dat <Sky Dat>
2015-12-19 18:55:34 +0000

    New formatted input class

---
+++ src/streams/FormattedInput.java
@@ -0,0 +1,16 @@
+package streams;
+import java.io.StreamTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+/*
+ * Created by skyda on 19/12/2015.
+ */
+public class FormattedInput {
+
+
+
+
+}
